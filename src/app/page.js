4873047ca08d629a01b7552b597cb280815bb842
SurajRawat12345 <rawatsuraj2612@gmail.com>
2026-08-08 12:53:36 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,15 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/Hero";
 import ProductShowcase from "@/components/product-showcase";
-import ImageGallery from "@/components/Image-galary";
-
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
-      
+
       {/* Main Content */}
       <main className="flex-grow">
         <HeroSection />
         <ProductShowcase />
-        <ImageGallery />
       </main>
 
       <Footer />
